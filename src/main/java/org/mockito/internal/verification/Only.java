5fe1060ebe12d0eed2469199958981cc4d472c25
Piotr Przybylak <piotr.przybylak@gmail.com>
2024-03-02 14:06:41 +0100

    Refactor: reuse other verifiers

--- src/main/java/org/mockito/internal/verification/Only.java
+++ src/main/java/org/mockito/internal/verification/Only.java
@@ -4,37 +4,19 @@
  */
 package org.mockito.internal.verification;
 
-import static org.mockito.internal.exceptions.Reporter.noMoreInteractionsWanted;
-import static org.mockito.internal.exceptions.Reporter.wantedButNotInvoked;
-import static org.mockito.internal.invocation.InvocationMarker.markVerified;
-import static org.mockito.internal.invocation.InvocationsFinder.findFirstUnverified;
-import static org.mockito.internal.invocation.InvocationsFinder.findInvocations;
-
-import java.util.List;
-
 import org.mockito.internal.verification.api.VerificationData;
-import org.mockito.invocation.Invocation;
-import org.mockito.invocation.MatchableInvocation;
 import org.mockito.verification.VerificationMode;
 
 public class Only implements VerificationMode {
 
+    private final Times once = new Times(1);
+
+    private final NoMoreInteractions noMoreInteractions = new NoMoreInteractions();
+
     @Override
-    @SuppressWarnings("unchecked")
     public void verify(VerificationData data) {
-        MatchableInvocation target = data.getTarget();
-        List<Invocation> invocations = data.getAllInvocations();
-        List<Invocation> chunk = findInvocations(invocations, target);
-        if(!chunk.isEmpty()) {
-            markVerified(chunk.get(0), target);
-        }
-        if (invocations.size() != 1 && !chunk.isEmpty()) {
-            Invocation unverified = findFirstUnverified(invocations);
-            throw noMoreInteractionsWanted(unverified, (List) invocations);
-        }
-        if (invocations.size() != 1 || chunk.isEmpty()) {
-            throw wantedButNotInvoked(target);
-        }
+        once.verify(data);
+        noMoreInteractions.verify(data);
     }
 
     @Override
